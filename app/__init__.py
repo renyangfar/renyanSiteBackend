37357a9a4537aff6126b5ab22ee65c297c02a0cf
renyan <renyangfar@gmail.com>
2019-02-25 14:23:58 +0800

    register func

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 import os
 
 from flask import Flask
-from user import user
-from main import main
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../sqlite.db'
+
+db = SQLAlchemy(app)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+from user import user
+from main import main
 
 app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(user, url_prefix='/user')
--- app/user/views.py
+++ app/user/views.py
@@ -1,18 +1,35 @@
-from flask import url_for, flash, request
-from werkzeug.utils import redirect
+import json
+import re
 
-from models import User
-from . import user, db
+from flask import request
+
+from app import db
+from .models import User
+from . import user
 
 
 @user.route('/register', methods=['GET', 'POST'])
 def register():
-    if True:
-        register_data = request.form
-        user = User(email=register_data.get('Email'), username=register_data.get('Username'), password=register_data.get("Password"))
+    try:
+        req_data = request.form
+        email, username, password, password_confirm = req_data.get('email', ''), req_data.get('username', ''), \
+                                                      req_data.get('password', ''), req_data.get('password_confirm', '')
+        if not (email and username and password and password_confirm):
+            return json.dumps({"success": False, "data": "invalid field"})
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            return json.dumps({"success": False, "data": "invalid email format"})
+        if not len(username) >= 4:
+            return json.dumps({"success": False, "data": "invalid username format"})
+        if not len(password) >= 4:
+            return json.dumps({"success": False, "data": "invalid password format"})
+        if not password == password_confirm:
+            return json.dumps({"success": False, "data": "invalid password_confirm format"})
+        if User.query.filter_by(username=username).first():
+            return json.dumps({"success": False, "data": "username exist"})
+        user = User(username=username, password=password, email=email)
         db.session.add(user)
         db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
-    return 'invalidate form'
-    # return render_template('auth/register.html', form=form)
+        return json.dumps({"success": True, "data": "register success"})
+    except Exception as e:
+        print(e)
+        return json.dumps({"success": False, "data": e.message})
