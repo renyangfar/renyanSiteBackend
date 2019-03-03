55edf52524a410f313abd5fa440684b5727eb047
renyan <renyangfar@gmail.com>
2019-03-03 18:54:52 +0800

    前后端首次联调

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,7 +12,7 @@ app.secret_key = Flask.secret_key
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
-login_manager.login_view = "/"
+login_manager.login_view = "/loginRegister"
 login_manager.init_app(app)
 
 
@@ -23,6 +23,10 @@ from main import main
 from user import user
 from blog import blog
 
-app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(main, url_prefix='/main')
 app.register_blueprint(user, url_prefix='/user')
 app.register_blueprint(blog, url_prefix='/blog')
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Welcome")
